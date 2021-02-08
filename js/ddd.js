1ccaa411f852d44368b5d3a09f4a04bfcc37f809
NiedsonEmanoel <niedsonemanoeltbm@gmail.com>
2021-02-08 15:34:02 -0300

    Delete ddd.js

--- js/ddd.js
+++
@@ -1,8 +0,0 @@
-function s() {
-let NomeContato = "Jooj";
-let TelefoneContato = "8787878";
-let CpfContato = "55555"
-let query = 'update Cadastro set Nome = "'+NomeContato+'" where Telefone = "'+TelefoneContato+'" AND CPF = "'+CpfContato+'"'
-console.log(query);
-}
-s();
