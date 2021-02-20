3f56a689483469818a9a00b31be980dc07ab3aaf
NiedsonEmanoel <niedsonemanoeltbm@gmail.com>
2021-02-20 19:29:05 -0300

    Update dialogflowWebhook.js

--- js/routes/dialogflowWebhook.js
+++ js/routes/dialogflowWebhook.js
@@ -3,13 +3,13 @@ const router = express.Router();
 router.use(express.urlencoded());
 router.use(express.json());
 
-router.post("/", (request, response) => {
-    let intentName = request.body.queryResult.intent.displayName;
+router.post("/", async(request, response) => {
+    let intentName = await request.body.queryResult.intent.displayName;
     // Teste Webhook
     if (intentName == "teste") {
-        response.json({ fulfillmentText: `Webhook Funcionando!!!` });
+         response.json({ fulfillmentText: `Webhook Funcionando!!!` });
     }
-    // Soma
+    // Somas
     else if (intentName == "soma") {
         let n1 = request.body.queryResult.parameters["num1"];
         let n2 = request.body.queryResult.parameters["num2"];
