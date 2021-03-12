83c90fe10c45b2c2d47a44db174bceaf18c68812
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2021-03-12 12:54:47 -0300

    criação da view em react

--- src/Middlewares/idPasser.js
+++ src/Middlewares/idPasser.js
@@ -1,27 +1,31 @@
 let sessions = require('../Controllers/multisession.controller');
 
 module.exports = (req, res, next) => {
-    let reqAuth = req.query.id;
-    let max = sessions.getSessions() + 1;
-    let sessionsX = [];
+    let idText = req.query.id;
+    let limit = sessions.getLimit();
+    let idNumber;
 
-    for (let i = 1; i < max; i++) {
-        sessionsX.push(i);
+    try {
+        idNumber = new Number(idText);
+    } catch{
+        res.status(400).send({
+            "error": "ID inválido"
+        });
     }
 
-    function verify(t) {
-        for (let key in sessionsX) {
-            if (sessionsX[key] == t) { return true; }
+    let started = sessions.getSessions();
+
+    if ((idNumber >= 0) && (idNumber < limit)) {
+        if (started.includes(idText)) {
+            next();
+        } else {
+            res.status(400).send({
+                "error": "Sessão não inicializada"
+            });
         }
-        return false;
-    }
-
-    if (!reqAuth) {
-        return res.status(400).send({ error: "ID não informado." });
-    }
-    if ( ( (reqAuth >= 0) && (verify(reqAuth) ) ) || (reqAuth==0)) {
-        return next();
     } else {
-        return res.status(400).send({ error: "ID inválido" });
+        res.status(400).send({
+            "error": "ID inválido"
+        });
     }
 }
