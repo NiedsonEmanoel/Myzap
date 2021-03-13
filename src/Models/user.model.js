67f53e2debba1bc0fc75322f7786c56b73f88278
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2021-03-13 18:55:24 -0300

    atualizações

--- src/Models/user.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = require('./Schema/UserSchema');
-
-const User = mongoose.model('users', UserSchema);
-
-module.exports = User;
--- src/Views/src/pages/admin/dashboard/index.js
+++ src/Views/src/pages/admin/dashboard/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Box from '@material-ui/core/Box';
@@ -14,7 +13,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit">
-        Niedson Emanoel
+        Niedson Emanoel & Apoastro
       </Link>{' '}
       {new Date().getFullYear()}
       {''}
