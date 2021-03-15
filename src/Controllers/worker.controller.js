143328dd747b84429ff5124147350eb6b682cac9
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2021-03-15 17:30:46 -0300

    atualização

--- src/Controllers/worker.controller.js
+++ src/Controllers/worker.controller.js
@@ -1,4 +1,6 @@
 const Workers = require('../Models/worker.model');
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET;
 
 module.exports = {
     async index(req, res, next) {
@@ -98,5 +100,58 @@ module.exports = {
         } catch (error) {
             next(error);
         }
+    },
+
+    async login(req, res) {
+        const { email, senha } = req.body;
+        Usuario.findOne({ email_usuario: email }, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.status(200).json({ erro: "Erro no servidor. Por favor, tente novamente" });
+            } else if (!user) {
+                res.status(200).json({ status: 2, error: 'E-mail não encontrado no banco de dados' });
+            } else {
+                user.isCorrectPassword(senha, async function (err, same) {
+                    if (err) {
+                        res.status(200).json({ error: "Erro no servidor. Por favor, tente novamente" });
+                    } else if (!same) {
+                        res.status(200).json({ status: 2, error: "A senha não confere" });
+                    } else {
+                        const payload = { email };
+                        const token = jwt.sign(payload, secret, {
+                            expiresIn: '24h'
+                        })
+                        res.cookie('token', token, { httpOnly: true });
+                        res.status(200).json({ status: 1, auth: true, token: token, id_client: user._id, user_name: user.nome_usuario, user_type: user.tipo_usuario });
+                    }
+                })
+
+            }
+        })
+    },
+
+    async checkToken(req, res) {
+        const token = req.body.token || req.query.token || req.cookies.token || req.headers['x-access-token'] || req.headers.authorization;
+        if (!token) {
+            res.json({ status: 401, msg: 'Não autorizado: Token inexistente!' });
+        } else {
+            jwt.verify(token, secret, function (err, decoded) {
+                if (err) {
+                    res.json({ status: 401, msg: 'Não autorizado: Token inválido!' });
+                } else {
+                    res.json({ status: 200 })
+                }
+            })
+        }
+    },
+
+    async destroyToken(req, res) {
+        const token = req.headers.token;
+        if (token) {
+            res.cookie('token', null, { httpOnly: true });
+        } else {
+            res.status(401).send("Logout não autorizado!")
+        }
+        res.send("Sessão finalizada com sucesso!");
     }
 }
--- src/Routes/apis/api.js
+++ src/Routes/apis/api.js
@@ -5,3 +5,13 @@ api.use('/', require('./rest/rest.route'));
 api.use('/whatsapp', require('./venom/venom.route'));
 
 module.exports = api;
+
+
+/*
+
+routes.post('/api/usuarios/login',Usuario.login);
+routes.get('/api/usuarios/checktoken',Usuario.checkToken);
+routes.get('/api/usuarios/destroytoken',Usuario.destroyToken);
+
+
+*/
