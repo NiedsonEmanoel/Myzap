a7af3ee3838304761b145cf4cc3943f9f5b37675
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2021-03-19 10:13:02 -0300

    up

--- src/Routes/apis/rest/messages.route.js
+++ src/Routes/apis/rest/messages.route.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const messageController = require('../../../Controllers/messages.controller');
 const Router = express.Router();
 
-
+Router.get('/', messageController.index);
+Router.get('/:chatId', messageController.details);
 
 module.exports = Router;
