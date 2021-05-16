c6c0976392ad3c0e9f840c65398d9cf379a1fd1b
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2021-05-16 17:22:14 -0300

    docker changes

--- src/Controllers/multisession.controller.js
+++ src/Controllers/multisession.controller.js
@@ -10,11 +10,19 @@ const messageHelper = require('./messages.controller');
 
 module.exports = {
     async createInternal() {
-        let dir = process.env.JSON_LOCATION;
-        if (!dir) {
-            dir = path.resolve(__dirname, '../', 'dialogflow.json');
+        let privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
+        const internalCredential = {
+            "type": "service_account",
+            "project_id": process.env.PROJECT_ID,
+            "private_key_id": process.env.PRIVATE_KEY_ID,
+            "private_key": privateKey,
+            "client_email": process.env.CLIENT_EMAIL,
+            "client_id": process.env.CLIENT_ID,
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
         }
-        const internalCredential = require(path.resolve(dir));
         if (!limit) {
             limit = 1;
         }
